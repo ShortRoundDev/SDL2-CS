770c3435b3d5854eae427ef5534628bd3734bf81
collinoswalt <collin.d.oswalt@gmail.com>
2019-11-25 19:48:26 -0500

    added custom binding for custom SDL functions

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -2018,12 +2018,39 @@ namespace SDL2
 			ref SDL_Rect dstrect
 		);
 
-		/* renderer refers to an SDL_Renderer*, texture to an SDL_Texture*.
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int SDL_RenderCopyMany(
+			IntPtr renderer,
+			IntPtr texture,
+			SDL_Rect[] srcrect,
+			SDL_Rect[] dstrect,
+            int Size
+		);
+
+        [DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SDL_RenderCopyMany(
+            IntPtr renderer,
+            IntPtr texture,
+            IntPtr srcrect,
+            SDL_Rect[] dstrect,
+            int Size
+        );
+
+        [DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SDL_RenderCopyMany(
+            IntPtr renderer,
+            IntPtr texture,
+            IntPtr srcrect,
+            IntPtr dstrect,
+            int Size
+        );
+
+        /* renderer refers to an SDL_Renderer*, texture to an SDL_Texture*.
 		 * Internally, this function contains logic to use default values when
 		 * source and destination rectangles are passed as NULL.
 		 * This overload allows for IntPtr.Zero (null) to be passed for srcrect.
 		 */
-		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern int SDL_RenderCopy(
 			IntPtr renderer,
 			IntPtr texture,
